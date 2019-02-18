7031e640420bc4e5e3d35d78478438a1977ab96a
xpatmah <mchpathak@gmail.com>
2019-02-19 00:17:03 +0530

    first commit of oracle

--- hr_packages.sql
+++ hr_packages.sql
@@ -8,4 +8,6 @@ g_inactive_status constant varchar2(1):='A';
 
 cursor get_employee is select employee_id from employees;
 
+function calc_bonus(p_department_id number) return number;
+
 end newpackage;
