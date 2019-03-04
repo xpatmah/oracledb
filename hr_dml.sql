53b6d3d87b5200dff2c5c2c00bd22f200b5efe02
xpatmah <mchpathak@gmail.com>
2019-03-04 06:57:29 +0530

    Sql Practice

--- hr_dml.sql
+++ hr_dml.sql
@@ -62,3 +62,20 @@ create table org_table(
   constraint primary_key_c primary key (org_id , org_ceo),
   constraint forign_key foreign key(org_location) references locations(location_id) on delete cascade
 )
+
+create table org_table(
+  org_id number not null unique,
+  org_name varchar(30) constraint not_null2 unique,
+  org_ceo varchar(30),
+  no_of_emp number constraint not_null not null,
+  org_location number,
+  org_city_code number,
+  org_operational_currency_code number,
+  unique(org_location),
+  constraint unique_more_column unique (org_ceo,org_location),
+  constraint primary_key_c primary key (org_id , org_ceo),
+  constraint forign_key foreign key(org_location) references locations(location_id) on delete cascade,
+  constraint min_emp check(no_of_emp>3000)
+)
+
+drop table org_table;
